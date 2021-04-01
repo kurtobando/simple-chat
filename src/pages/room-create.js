8bf2a923496f3f3633665fb0b59e9d8330c50c85
Kurt Obando <kurtobando.developer@gmail.com>
2021-04-01 17:24:05 +0800

    add password for protected rooms

--- src/pages/room-create.js
+++ src/pages/room-create.js
@@ -6,15 +6,40 @@ const RoomCreate = () => {
         <div className="container-fluid">
             <div>
                 <form>
+                    {/* room name */}
                     <div className="form-group">
                         <input className="form-control" type="text" placeholder="Room Name" />
                     </div>
+
+                    {/* room description */}
                     <div className="form-group">
                         <input className="form-control" type="text" placeholder="Room Description" />
                     </div>
+
+                    {/* room unique id */}
                     <div className="form-group">
                         <input className="form-control" type="text" placeholder="Room Unique ID" readOnly={true} />
                     </div>
+
+                    {/* enable protected room */}
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" value="" id="enable-protected-room" />
+                        <label className="form-check-label" htmlFor="enable-protected-room">
+                            Enable Protected Room?
+                        </label>
+                    </div>
+
+                    {/* password */}
+                    <div className="form-group">
+                        <input className="form-control" type="text" placeholder="Password" />
+                    </div>
+
+                    {/* retype password */}
+                    <div className="form-group">
+                        <input className="form-control" type="text" placeholder="Re-type Password" />
+                    </div>
+
+                    {/* buttons */}
                     <div className="form-group text-center">
                         <button className="m-1 btn btn-primary" type="submit">
                             Create
--- src/pages/room-join.js
+++ src/pages/room-join.js
@@ -6,9 +6,25 @@ const RoomJoin = () => {
         <div className="container-fluid">
             <div>
                 <form>
+                    {/* room unique id */}
                     <div className="form-group">
                         <input className="form-control" type="text" placeholder="Room Unique ID" />
                     </div>
+
+                    {/* has password? */}
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" value="" id="enable-protected-room" />
+                        <label className="form-check-label" htmlFor="enable-protected-room">
+                            Room has password?
+                        </label>
+                    </div>
+
+                    {/* password */}
+                    <div className="form-group">
+                        <input className="form-control" type="text" placeholder="Password" />
+                    </div>
+
+                    {/* buttons */}
                     <div className="form-group text-center">
                         <button className="m-1 btn btn-primary" type="submit">
                             Create
