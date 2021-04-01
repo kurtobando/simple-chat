1c1557effed6db2a6e9be2e729b38307cceef6e1
Kurt Obando <kurtobando.developer@gmail.com>
2021-04-01 16:38:06 +0800

    add jquery, bootstrap, and popper.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  siteMetadata: {
-    title: "Simple Chat",
-    description: "Simple Chat, made from ReactJS, and socket.io",
-  },
-  plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
+    siteMetadata: {
+        title: "Simple Chat",
+        description: "Simple Chat, made from ReactJS, and socket.io",
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
-};
+    plugins: [
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.png",
+            },
+        },
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+    ],
+}
