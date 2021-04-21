ec85ba4e17324c914cf9bacfafa403b195317aa2
Kurt Obando <kurtobando.developer@gmail.com>
2021-04-21 11:35:18 +0800

    add clearInterval when dashboard/index component will unmount

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -5,17 +5,25 @@ import store from "store2"
 import { navigate } from "gatsby"
 
 class Index extends React.Component {
+    constructor() {
+        super()
+        this.interval = null
+    }
     componentDidMount() {
         // if room id is not present, redirect to index page
         if (!store("room_id")) {
             return navigate("/?authenticated=false")
         }
-        setInterval(() => {
+        this.interval = setInterval(() => {
             if (!store("room_id")) {
                 return navigate("/?authenticated=false")
             }
         }, 1000)
     }
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
+
     render() {
         return (
             <Layout>
