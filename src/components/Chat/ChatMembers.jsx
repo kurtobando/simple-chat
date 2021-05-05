93c70a603b2914cce3a413db403b037dca6f8026
Kurt Obando <kurtobando.developer@gmail.com>
2021-05-05 18:17:33 +0800

    add text for avatar component, with center margin

--- src/components/Chat/ChatMembers.jsx
+++ src/components/Chat/ChatMembers.jsx
@@ -4,8 +4,9 @@ const styles = {
     wordWrap: "break-word",
     borderRadius: "50%",
     textAlign: "center",
-    height: "6em",
-    width: "6em",
+    height: "5.5em",
+    width: "5.5em",
+    margin: "5px auto 0px auto",
 }
 
 const ChatMembers = ({ state }) => {
@@ -17,8 +18,13 @@ const ChatMembers = ({ state }) => {
         const imageSource = `https://avatars.dicebear.com/api/human/${name}.svg`
 
         return (
-            <div key={id} className="bg-white  m-1 small p-2 " style={styles}>
-                <img src={imageSource} alt={name} />
+            <div>
+                <div key={id} className="bg-white small p-2 " style={styles}>
+                    <img src={imageSource} alt={name} style={{ height: "40px", width: "40px" }} />
+                </div>
+                <span className="text-info m-auto d-block p-1 small text-center rounded" style={{ fontSize: "0.6em" }}>
+                    {name}
+                </span>
             </div>
         )
     })
