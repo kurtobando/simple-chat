5e24e9bf3c092b36ef6f70c2daef803cd4fb9d4b
Kurt Obando <kurtobando.developer@gmail.com>
2021-05-06 12:24:07 +0800

    update 404 component, with h1 and p elements, with a better text description

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,10 +3,17 @@ import { Link } from "gatsby"
 
 const NotFoundPage = () => {
     return (
-        <main>
-            <title>Not found</title>
-            <Link to="/">Go home</Link>.
-        </main>
+        <div className="text-center bg-light vh-100">
+            <div className="d-flex flex-column justify-content-center" style={{ height: "inherit" }}>
+                <div>
+                    <h1>Sorry, Page Not found</h1>
+                    <p className="mr-5 ml-5">
+                        Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first
+                        place? Let's go <Link to="/">home</Link>. and try from there.
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
 
