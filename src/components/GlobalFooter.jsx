13340e7d37aeae31f0d6ffd806bd3031030e1e19
Kurt Obando <kurtobando.developer@gmail.com>
2021-05-06 16:14:30 +0800

    add footer and header content

--- src/components/GlobalFooter.jsx
+++ src/components/GlobalFooter.jsx
@@ -1,7 +1,17 @@
 import React from "react"
 
 const GlobalFooter = () => {
-    return <div></div>
+    return (
+        <div className="text-center text-muted small p-3">
+            <a href="https://github.com/kurtobando/" className="text-muted" target="_blank" rel="noreferrer">
+                Simple Chat - Github Repository
+            </a>
+            {` | `}
+            <a href="https://kurtobando.com/" className="text-muted" target="_blank" rel="noreferrer">
+                Kurt Obando
+            </a>
+        </div>
+    )
 }
 
 export default GlobalFooter
