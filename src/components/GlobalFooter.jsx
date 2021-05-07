88b0ac1644f389d686f98d245fc58eac116b26fe
Kurt Obando <kurtobando.developer@gmail.com>
2021-05-07 18:01:53 +0800

    quick update for the GlobalFooter component

--- src/components/GlobalFooter.jsx
+++ src/components/GlobalFooter.jsx
@@ -3,12 +3,8 @@ import React from "react"
 const GlobalFooter = () => {
     return (
         <div className="text-center text-muted small p-3">
-            <a href="https://github.com/kurtobando/simple-chat" className="text-muted" target="_blank" rel="noreferrer">
-                Simple Chat - Github Repository
-            </a>
-            {` | `}
             <a href="https://kurtobando.com/" className="text-muted" target="_blank" rel="noreferrer">
-                Kurt Obando
+                Simple Chat by Kurt Obando
             </a>
         </div>
     )
